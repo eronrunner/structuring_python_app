0ec9abec7f23547a290494161e679b14e9a3b8e0
eron.runner@gmail.com <eron.runner@gmail.com>
2020-07-20 18:54:59 +0700

    FlaskApp: add logging config

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,10 +1,15 @@
 import os
+import logging.config
 
 APP_SETTINGS = os.environ.get('APP_SETTINGS', 'config.DevelopmentConfig')
 DEV_SERVER_NAME = os.environ.get('DEV_SERVER_NAME', 'localhost:5000')
+LOGGER = os.environ.get('APP_LOGGER', 'dev')
 
 # JSON Encoder
 
 # BaseConverter
 
-# TODO LOG
+# logging
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger(LOGGER)
